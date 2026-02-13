8533510808d7c098ac1df1ebb176ba9a85ead5e1
vanshika2621 <vanshikagiri2020@gmail.com>
2026-02-13 21:53:16 +0530

    Create Day13x1.c

---
+++ Day13x1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+
+    // Input matrix
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    while(top <= bottom && left <= right) {
+
+        // Traverse Top Row
+        for(int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+
+        // Traverse Right Column
+        for(int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+
+        // Traverse Bottom Row
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse Left Column
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
