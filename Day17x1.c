8477b973376bb68273164c07ef5885a32e17b701
vanshika2621 <vanshikagiri2020@gmail.com>
2026-02-17 17:47:54 +0530

    Create Day17x1.c

---
+++ Day17x1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Minimum value = %d\n", min);
+    printf("Maximum value = %d\n", max);
+
+    return 0;
+}
