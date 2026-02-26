9ce064ab7883ba0849fed6f053126ad5bf44928c
vanshika2621 <vanshikagiri2020@gmail.com>
2026-02-26 17:58:25 +0530

    Create Day26x2.c

---
+++ Day26x2.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int val;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    Node* head;
+    int size;
+} MyLinkedList;
+
+/** Initialize your data structure here. */
+MyLinkedList* myLinkedListCreate() {
+    MyLinkedList* obj = (MyLinkedList*)malloc(sizeof(MyLinkedList));
+    obj->head = NULL;
+    obj->size = 0;
+    return obj;
+}
+
+/** Get the value of the index-th node in the linked list. If invalid, return -1. */
+int myLinkedListGet(MyLinkedList* obj, int index) {
+    if (index < 0 || index >= obj->size)
+        return -1;
+
+    Node* temp = obj->head;
+    for (int i = 0; i < index; i++)
+        temp = temp->next;
+
+    return temp->val;
+}
+
+/** Add a node of value val before the first element. */
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = obj->head;
+
+    obj->head = newNode;
+    obj->size++;
+}
+
+/** Append a node of value val to the last element. */
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = NULL;
+
+    if (obj->head == NULL) {
+        obj->head = newNode;
+    } else {
+        Node* temp = obj->head;
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = newNode;
+    }
+
+    obj->size++;
+}
+
+/** Add a node of value val before the index-th node. */
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+    if (index < 0 || index > obj->size)
+        return;
+
+    if (index == 0) {
+        myLinkedListAddAtHead(obj, val);
+        return;
+    }
+
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->val = val;
+
+    Node* temp = obj->head;
+    for (int i = 0; i < index - 1; i++)
+        temp = temp->next;
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    obj->size++;
+}
+
+/** Delete the index-th node in the linked list. */
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+    if (index < 0 || index >= obj->size)
+        return;
+
+    Node* temp = obj->head;
+
+    if (index == 0) {
+        obj->head = temp->next;
+        free(temp);
+    } else {
+        for (int i = 0; i < index - 1; i++)
+            temp = temp->next;
+
+        Node* deleteNode = temp->next;
+        temp->next = deleteNode->next;
+        free(deleteNode);
+    }
+
+    obj->size--;
+}
+
+/** Free entire list */
+void myLinkedListFree(MyLinkedList* obj) {
+    Node* temp = obj->head;
+    while (temp != NULL) {
+        Node* nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+    free(obj);
+}
