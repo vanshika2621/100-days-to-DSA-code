d4520248ffbf57f48764a843aa15b3fbc552c960
vanshika2621 <vanshikagiri2020@gmail.com>
2026-03-23 22:21:51 +0530

    Create Day51x2.c

---
+++ Day51x2.c
@@ -0,0 +1,11 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root) {
+        if (p->val < root->val && q->val < root->val)
+            root = root->left;
+        else if (p->val > root->val && q->val > root->val)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
