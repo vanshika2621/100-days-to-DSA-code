0b0ef8165485a4fd2683e5ad3b23b206fda50e94
vanshika2621 <vanshikagiri2020@gmail.com>
2026-03-29 21:10:08 +0530

    Create Day57x2.c

---
+++ Day57x2.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(n * sizeof(struct TreeNode*));
+    int front = 0, rear = 0;
+
+    struct TreeNode* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        struct TreeNode* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+void flatten(struct TreeNode* root) {
+    if (!root) return;
+
+    flatten(root->left);
+    flatten(root->right);
+
+    struct TreeNode* left = root->left;
+    struct TreeNode* right = root->right;
+
+    root->left = NULL;
+    root->right = left;
+
+    struct TreeNode* curr = root;
+    while (curr->right) curr = curr->right;
+
+    curr->right = right;
+}
+
+void printList(struct TreeNode* root) {
+    while (root) {
+        printf("%d ", root->val);
+        root = root->right;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    flatten(root);
+
+    printList(root);
+
+    free(arr);
+    return 0;
+}
