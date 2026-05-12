bd09c8df614896d961631ed43ab4aaf0ab452126
vanshika2621 <vanshikagiri2020@gmail.com>
2026-05-12 22:17:39 +0530

    Create Day93x1.c

---
+++ Day93x1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value)
+{
+    if(top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        top++;
+        stack[top] = value;
+        printf("%d pushed into stack\n", value);
+    }
+}
+
+// Pop operation
+void pop()
+{
+    if(top == -1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+// Display stack
+void display()
+{
+    int i;
+
+    if(top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Stack elements are:\n");
+
+        for(i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    pop();
+
+    display();
+
+    return 0;
+}
